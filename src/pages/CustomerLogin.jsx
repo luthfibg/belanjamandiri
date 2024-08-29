59bdaaf815b9519b85942f33e3388273eface3c3
luthfibg <luthfinewzoneonline@gmail.com>
2024-08-29 15:50:29 +0700

    #41 create new appbar for detail product page

--- src/pages/CustomerLogin.jsx
+++ src/pages/CustomerLogin.jsx
@@ -76,3 +76,36 @@ const CustomerLogin = () => {
 }
 
 export default CustomerLogin;
+
+// import * as React from 'react';
+// import { AppProvider, SignInPage } from '@toolpad/core';
+// import { useTheme } from '@mui/material/styles';
+// // preview-start
+// const providers = [
+//   { id: 'github', name: 'GitHub' },
+//   { id: 'google', name: 'Google' },
+//   { id: 'facebook', name: 'Facebook' },
+// ];
+
+// // preview-end
+
+// const signIn = async (provider) => {
+//   const promise = new Promise((resolve) => {
+//     setTimeout(() => {
+//       console.log(`Sign in with ${provider.id}`);
+//       resolve();
+//     }, 500);
+//   });
+//   return promise;
+// };
+
+// export default function OAuthSignInPage() {
+//   const theme = useTheme();
+//   return (
+//     // preview-start
+//     <AppProvider theme={theme}>
+//       <SignInPage signIn={signIn} providers={providers} />
+//     </AppProvider>
+//     // preview-end
+//   );
+// }
--- src/pages/DetailProduct.jsx
+++ src/pages/DetailProduct.jsx
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import { Box, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
+import CustomAppBar2 from "../components/CustomAppBar2";
 
 const DetailProduct = () => {
   const { productId } = useParams();
   const [product, setProduct] = useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
   useEffect(() => {
-    console.log('productId from useParams:', productId); // Tambahkan logging di sini
     const fetchProductDetails = async () => {
       try {
         const response = await axios.get(`http://localhost:2999/data/products/${productId}`);
-        console.log('Product data:', response.data); // Tambahkan logging di sini
         setProduct(response.data);
       } catch (error) {
         console.error("Error fetching product details:", error);
@@ -26,17 +27,41 @@ const DetailProduct = () => {
     return <div>Loading...</div>;
   }
 
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const logout = () => {
+    // Remove the JWT token from localStorage
+    localStorage.removeItem('token');
+    localStorage.removeItem('username');
+    // Redirect the user to the login page
+    window.location.href = '/login-customer';
+  }
+
+
   return (
     <>
-    <Box>
-        <Typography variant="h4">Product Details</Typography>
-        <Typography variant="h5">{product.product_type}</Typography>
-        <img src={product.product_image_1} alt={product.product_type} />
-        <Typography variant="body1">Price: Rp {product.product_price}</Typography>
-        <Typography variant="body1">Category: {product.product_cat}</Typography>
-        <Typography variant="body1">Subcategory: {product.product_subcat}</Typography>
-        <Typography variant="body1">{product.product_description}</Typography>
-    </Box>
+    <CustomAppBar2
+     handleOpenUserMenu={handleOpenUserMenu}
+     anchorElUser={anchorElUser}
+     handleCloseUserMenu={handleCloseUserMenu}
+     settings={settings}
+     logout={logout}
+     />
+    <Container sx={{ mt: 10 }}>
+      <Typography variant="h4">Product Details</Typography>
+      <Typography variant="h5">{product.product_type}</Typography>
+      <img src={product.product_image_1} alt={product.product_type} width={"30%"} />
+      <Typography variant="body1">Price: Rp {product.product_price}</Typography>
+      <Typography variant="body1">Category: {product.product_cat}</Typography>
+      <Typography variant="body1">Subcategory: {product.product_subcat}</Typography>
+      <Typography variant="body1">{product.product_description}</Typography>
+    </Container>
     </>
   );
 };
